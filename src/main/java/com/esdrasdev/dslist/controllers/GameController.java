85b479b4cc95203488c3d8f9eb42bad35a9c8693
Esdras Pinheiro <esdras.dev@hotmail.com>
2023-07-13 20:48:39 -0300

    GameController, Dto, Repository, Service e properties

---
+++ src/main/java/com/esdrasdev/dslist/controllers/GameController.java
@@ -0,0 +1,24 @@
+package com.esdrasdev.dslist.controllers;
+
+import com.esdrasdev.dslist.dto.GameMinDTO;
+import com.esdrasdev.dslist.services.GameService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/games")
+public class GameController {
+
+    @Autowired
+    private GameService gameService;
+
+    @GetMapping
+    public List<GameMinDTO> findAll() {
+        List<GameMinDTO> result = gameService.findAll();
+        return result;
+    }
+}
